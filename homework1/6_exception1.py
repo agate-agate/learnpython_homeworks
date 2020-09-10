21728f1dba0dc7c77369e6bdc316d89cee70ebb4
Michael Alekseevich <sapphire.agate.agate@gmail.com>
2020-09-10 23:49:24 +0300

    Solution to the problem 6_exeption1

--- homework1/6_exception1.py
+++ homework1/6_exception1.py
@@ -4,17 +4,39 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию ask_user() из задания while2, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию ask_user() из задания while2, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
 
 def ask_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    conversation_responses = {
+        '2 + 2?': '5',
+        'spam?': ('Vikings approve. ' * 8).strip(),
+        'wound?': 'But a flesh one!',
+        'rabbit?': 'Well, that\'s no ordinary rabbit! That\'s the most ' \
+            'foul, cruel, and bad-tempered rodent you ever set eyes on!',
+    }
+    while True:
+        try:
+            query = (str(input('\nЗадайте вопрос: '))).strip()
+
+            # Способ прекрарить диалог преждевременно.
+            if query.lower() == 'goodbye':
+                print('Пока!')
+                break
+
+            answer = str(conversation_responses.get(query, ''))
+
+            if (answer != ''):
+                print('Ответ: ' + answer)
+        except KeyboardInterrupt:
+            print('\nПока!')
+            break
+
 if __name__ == "__main__":
     ask_user()
