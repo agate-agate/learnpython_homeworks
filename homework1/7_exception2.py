6eff2c8781788de0432ebfd63f46125d5ec2d806
Michael Alekseevich <sapphire.agate.agate@gmail.com>
2020-09-10 23:59:26 +0300

    Solution to the problem 7_exeption2

--- homework1/7_exception2.py
+++ homework1/7_exception2.py
@@ -4,20 +4,30 @@
 
 Исключения: приведение типов
 
-* Напишите функцию get_summ(num_one, num_two), которая принимает 
-  на вход два целых числа (int), складывает их и возвращает результат 
+* Напишите функцию get_summ(num_one, num_two), которая принимает
+  на вход два целых числа (int), складывает их и возвращает результат
   сложения
-* Оба аргумента нужно приводить к целому числу при помощи int() и 
+* Оба аргумента нужно приводить к целому числу при помощи int() и
   перехватывать исключение ValueError если приведение типов не сработало
-    
+
 """
 
 def get_summ(num_one, num_two):
     """
     Замените pass на ваш код
     """
-    pass
-    
+
+    def to_int(num):
+        try:
+            num = int(num)
+        except ValueError:
+            num = 0;
+        return num
+
+    num_one = to_int(num_one);
+    num_two = to_int(num_two);
+    return num_one + num_two
+
 if __name__ == "__main__":
     print(get_summ(2, 2))
     print(get_summ(3, "3"))
