029ccdc893abde3e94f61e09ed2b1234bf12e3fc
Michael Alekseevich <sapphire.agate.agate@gmail.com>
2020-09-16 07:39:02 +0300

    Solution to the problem 1_date_and_time

--- homework2/1_date_and_time.py
+++ homework2/1_date_and_time.py
@@ -7,13 +7,19 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    now = datetime.now()
+    nice_format = '%Y-%m-%d'
+
+    print((now - timedelta(days=1) ).strftime(nice_format))
+    print((now                     ).strftime(nice_format))
+    print((now - timedelta(days=30)).strftime(nice_format))
 
 
 def str_2_datetime(date_string):
@@ -21,7 +27,7 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    return datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
 
 if __name__ == "__main__":
     print_days()
