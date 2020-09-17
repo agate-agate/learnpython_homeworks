4400150c370f705e7651dc7f4f52de277a1bc57b
Michael Alekseevich <sapphire.agate.agate@gmail.com>
2020-09-17 07:10:57 +0300

    Solution to the problem 2_files

--- homework2/2_files.py
+++ homework2/2_files.py
@@ -16,7 +16,18 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as file:
+        content = file.read()
+        print('Получаем контент файла как текст...готово.')
+
+        words = content.split()
+        word_count = len(words)
+        print(f'Количество слов в тексте: {word_count}')
+
+        content2 = content.replace('.', '!')
+        with open('referat2.txt', 'w', encoding='utf-8') as file2:
+            file2.write(content2)
+            print('Записываем новый контент в файл referat2.txt...готово.')
 
 if __name__ == "__main__":
     main()
